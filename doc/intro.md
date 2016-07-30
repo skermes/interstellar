0156f07c92668946f50ef2040e7e13daff6ccffc
Sean Kermes <sean@bigdatabio.com>
2016-07-30 17:26:15 -0400

    Initialize clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to interstellar
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject interstellar "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
